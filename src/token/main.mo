314210c4918c1262d10f6ceac1a09cfb8b3fe816
Code-Magician <priyanshkumar001@gmail.com>
2023-12-24 21:43:49 +0530

    Created a Owner Token account and added checkBalance Functionality using Principle ID

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,26 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+import Principal "mo:base/Principal";
+import Text "mo:base/Text";
+import HashMap "mo:base/HashMap";
+import Debug "mo:base/Debug";
+
+actor Bro {
+    var owner: Principal = Principal.fromText("bg65j-fvxzm-yw5y7-t233o-p6vac-n3fd5-hjhyc-xhmkr-r46rq-kiqja-hae");
+    var totalCoins: Nat = 1000000000;
+    var symbol: Text = "BRO";
+
+    var balanceOf = HashMap.HashMap<Principal, Nat>(1, Principal.equal, Principal.hash);
+    balanceOf.put(owner, totalCoins);
+
+    public query func getBalance(pId: Principal):async Nat {
+        let balance: Nat = switch(balanceOf.get(pId)) {
+            case null 0;
+            case (?result) result;
+        };
+
+        return balance;
     };
-};
+
+    public query func getSymbol(): async Text {
+        return symbol;
+    };
+} 
---
+++ src/token_assets/src/components/Balance.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { token } from "../../../declarations/token/index";
+import { Principal } from '@dfinity/principal';
+
+function Balance() {
+  const [balance, setBalance] = useState(0);
+  const [symbol, setSymbol] = useState("");
+  const [pId, setPId] = useState("");
+
+  useEffect(()=>{
+    (async ()=>{
+      setSymbol(await token.getSymbol());
+    })();
+  }, []);
+  
+   async function handleClick() {
+    const value = await token.getBalance(Principal.fromText(pId));
+    setBalance(value.toLocaleString());
+  }
+
+
+  return (
+    <div className="window white">
+      <label>Check account token balance:</label>
+      <p>
+        <input
+          id="balance-principal-id"
+          type="text"
+          placeholder="Enter a Principal ID"
+          onChange={(e)=>{
+            let value = e.target.value;
+            setPId(value);
+          }}
+          value={pId}
+        />
+      </p>
+      <p className="trade-buttons">
+        <button
+          id="btn-request-balance"
+          onClick={handleClick}
+        >
+          Check Balance
+        </button>
+      </p>
+      <p>This account has a balance of {balance}{symbol}.</p>
+    </div>
+  );
+}
+
+export default Balance;
